fd07e9363cf0fa324905fa0c7d9ca0e81b5d4cf0
1owenoreilly <owenoreilly488@gmail.com>
2024-09-24 13:22:46 -0400

    added instance variable address and added getter for it

--- src/studentexample/Student.java
+++ src/studentexample/Student.java
@@ -9,6 +9,7 @@ public class Student {
     
     private String name;
     private int age;
+    private String address;
 
     public Student(String name) {
         this.name = name;
@@ -30,5 +31,7 @@ public class Student {
         this.age = age;
     }
 
-    
+    public String getAddress(){
+        return this.address;
+    }
 }
