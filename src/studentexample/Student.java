a0ddc70e71a4373057f993910e992bf4e0b8aa13
1owenoreilly <owenoreilly488@gmail.com>
2024-09-24 12:43:32 -0400

    another commit

--- src/studentexample/Student.java
+++ src/studentexample/Student.java
@@ -29,6 +29,10 @@ public class Student {
     public void setAge(int age) {
         this.age = age;
     }
-
+    
+    @Override
+    public String toString(){
+        return "hi";
+    }
     
 }
